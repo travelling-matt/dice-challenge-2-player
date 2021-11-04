84fb95b32949d953dedf93a1c88ba0d69b0d6608
Matt Burton <mattwburton@gmail.com>
2021-11-04 12:20:15 +0000

    1 player game working

--- script/script.js
+++ script/script.js
@@ -1,51 +1,25 @@
-let resetBtn = document.getElementById("resetBtn");
-let player1 = document.getElementById("player1");
-let score = document.getElementById("score");
+let btnNewGame = document.getElementById("btnNewGame");
+let btnRollDice = document.getElementById("btnRollDice");
+let btnHold = document.getElementById("btnHold");
 let dice = document.getElementById("dice");
-let rollBtn = document.getElementById("rollBtn");
+let player1CurrentScore = document.getElementById("player1CurrentScore");
+let player2CurrentScore = document.getElementById("player2CurrentScore");
+let player1HoldScore = document.getElementById("player1HoldScore");
+let player2HoldScore = document.getElementById("player2HoldScore");
 let one = document.getElementById("one");
 let two = document.getElementById("two");
 let three = document.getElementById("three");
 let four = document.getElementById("four");
 let five = document.getElementById("five");
 let six = document.getElementById("six");
+/*i = player1 current score*/
 let i = 0
+/*j = dice roll*/
+/*k = player2 current score*/
 
-/*document.addEventListener("keydown", (e) => {
-    one.style.display = "none";
-    two.style.display = "none";
-    three.style.display = "none";
-    four.style.display = "none";
-    five.style.display = "none";
-    six.style.display = "none";
-    console.log(e);
-    console.log(e.key);
-    if (e.key == "r" || e.key == "R") {
-        let j = Math.ceil(Math.random()*6);
-        console.log(j)
-        i = i + j
-        console.log(i)
-        score.innerHTML = `<p>${i}</p>`;
-        if (j == 1) {
-            player1.innerHTML = `<p>You lose :(<br>Press s or click 'start again' to try again</p>`;
-            one.style.display = "block";
-            rollBtn.style.visibility = "hidden";
-            i = 0
-        } else if (j == 2) {      
-            two.style.display = "block";  
-        } else if (j == 3) {      
-            three.style.display = "block";  
-        } else if (j == 4) {      
-            four.style.display = "block";  
-        } else if (j == 5) {      
-            five.style.display = "block";  
-        } else if (j == 6) {      
-            six.style.display = "block";  
-        }
-    }
-});*/
 
-rollBtn.addEventListener("click", () => {
+
+btnRollDice.addEventListener("click", () => {
     one.style.display = "none";
     two.style.display = "none";
     three.style.display = "none";
@@ -57,18 +31,18 @@ rollBtn.addEventListener("click", () => {
     console.log(j)
     i = i + j
     console.log(i)
-    score.innerHTML = `<p>${i}</p>`;
+    player1CurrentScore.innerHTML = `<p>${i}</p>`;
         if (i >= 21) {
             player1.innerHTML = `<p>Winner!!!</p>`;
-            rollBtn.style.display = "none";
-            resetBtn.style.display = "block";
+            btnRollDice.style.visibility = "hidden";
+            btnNewGame.style.display = "block";
             winner.style.display = "block"; 
             i = 0
         } else if (j == 1) {
             player1.innerHTML = `<p>Loser :(</p>`;
             one.style.display = "block";
-            rollBtn.style.display = "none";
-            resetBtn.style.display = "block";
+            btnRollDice.style.display = "none";
+            btnNewGame.style.display = "block";
             /*loser.style.display = "block"; */
             i = 0
         } else if (j == 2) {      
@@ -85,15 +59,15 @@ rollBtn.addEventListener("click", () => {
     }
 )
 
-resetBtn.addEventListener("click", () => {
+btnNewGame.addEventListener("click", () => {
     location.reload();    
     }
 )
-document.addEventListener("keydown", (e) => {
+/*document.addEventListener("keydown", (e) => {
     if (e.key == "s" || e.key == "S") {
         location.reload();
     }
-});
+});*/
 
 /*dice.innerHTML = `<p>${document.getElementById("two")}</p>`;*/
 
